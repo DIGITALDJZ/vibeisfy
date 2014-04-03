5a05a9cb8c21279d0f4ac77bd7ec27ffca6576d5
KirilNN <kiril.nikolov@gmail.com>
2014-04-02 23:11:14 -0700

    add new vibe view, and description edit functionality

---
+++ Vibesify/NewVIbeViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewVIbeViewController.h
+//  Vibesify
+//
+//  Created by Kiril on 4/2/14.
+//  Copyright (c) 2014 vibesify. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewVIbeViewController : UIViewController
+
+@end
--- Vibesify/VibesListViewController.m
+++ Vibesify/VibesListViewController.m
@@ -137,10 +137,6 @@
     return cell;
 }
 
-- (IBAction)createNewVibeButtonPressed:(id)sender {
-    NSLog(@"test");
-}
-
 - (IBAction)timeButtonPressed:(id)sender {
     self.timeBorder.backgroundColor = lightGreenColor;
     [self.timeButtonOutlet setTitleColor:lightGreenColor forState:UIControlStateNormal];
